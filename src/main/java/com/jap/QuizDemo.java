e6a58210587ea665e79160a4f1b76f85db8a1ca9
HP <mdzaid1299@gmail.com>
2022-08-26 16:07:13 +0530

    Initial Commit

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -4,23 +4,44 @@ public class QuizDemo {
 
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
-    public String highestScore(String []nameOfSchool , String[] scores ){
+    public String highestScore(String[] nameOfSchool, String[] scores) {
+        int[] newScores = new int[scores.length];
+        String value = "";
+        try {
 
 
-        return "";
+            for (int i = 0; i < scores.length; i++) {
+                newScores[i] = Integer.parseInt(scores[i]);
+            }
+            int maxScore = 0;
+
+            for (int i = 0; i < nameOfSchool.length; i++) {
+                if (newScores[i] > maxScore) {
+                    maxScore = newScores[i];
+                    value = nameOfSchool[i];
+                }
+
+            }
+        } catch (NumberFormatException e) {
+            value = e.toString();
+        }
+        return value;
 
     }
+
     //convert all the names in uppercase
     //Handle the NullPointerException
-    public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+    public String[] convertAllNamesToCapital(String[] name) {
+        String[] upperCase = new String[name.length];
 
+        try {
+            for (int i = 0; i < name.length; i++) {
+                upperCase[i] = name[i].toUpperCase();
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
         return upperCase;
     }
 }
-
-
-
-
-
-
